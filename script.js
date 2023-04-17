ef40377157d1739c7657d8d89b52e4b98e1b5782
HeltonSantos0 <129709479+HeltonSantos0@users.noreply.github.com>
2023-04-17 01:50:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ function clicar(){
      pipe = window.document.querySelector('.pipe1')
      char = window.document.querySelector('.char')
    
-     const pipePosition = pipe.offsetLeft;
+     const pipePosition = pipe.offsetLeft
      const charPosition = +window.getComputedStyle(char).bottom.replace('px', '')
-      if ( !charPosition < 30 &&  !pipePosition >= 470 ) {
-     pipe.style.animation = 'pipe1-animation 1.7s infinite linear; '
-     pipe.style.left =`${pipePosition}px`
-     }else if (charPosition < 20 &&  pipePosition >= 460) 
+     if (charPosition < 50 && pipePosition >= 510 && pipePosition <= 760) 
       {    var rai = window.document.querySelector('#rai')
-      var GameOver = window.document.querySelector('#GameOver')
+      var GameOver = window.document.querySelector('.GameOver')
+      pipe.style.position = "absolute"
+      char.style.bottom = `${charPosition}px`
+      pipe.style.left =`${pipePosition}px`
       pipe.style.animation = 'none'
       rai.style.animation = 'rai-kua 4s linear'
       rai.style.display = 'block'
@@ -40,13 +40,14 @@ function clicar(){
 function voltar(){
      const blas = window.document.querySelector('.pipe1')
      const rais = window.document.querySelector("#rai")
-     const GO = window.document.querySelector('#GameOver')
+     const GO = window.document.querySelector('.GameOver')
      blas.style.animation = 'pipe-animation 1.7s infinite linear'
      rais.style.animation = 'none'
      rais.style.display = 'none'
      GO.style.animation = 'none'
      GO.style.display = 'none'
-     
+     char.style.position = "absolute"
+     char.style.bottom = "0%"
      var btnstart = document.querySelector("#btnstart")     
      
      btnstart = true     
@@ -95,8 +96,8 @@ function trocar4(){
      bonecos.style.display = "none"
      char.src = "raro.gif"
      char.style.position = "absolute"
-     char.style.bottom = "0px"
-     char.style.right = "0"
+     char.style.bottom = "0%%"
+     char.style.right = "6%"
      char.style.width = "30%"
     
 }
@@ -105,8 +106,8 @@ function trocar5(){
      bonecos.style.display = "none"
      char.src = "mew2.gif"
      char.style.position = "absolute"
-     char.style.bottom = "0px"
-     char.style.right = "6%"
+     char.style.bottom = "3%"
+     char.style.right = "10%"
      char.style.width = "20%"
     
 }
@@ -117,14 +118,6 @@ function Skin(){
 function OBS(){
      var bonecos2 =document.querySelector(".bonecos2")
      bonecos2.style.display = "block"
-}
-function trocab1(){
-     var bonecos2 =document.querySelector(".bonecos2")
-     pipe.src = "char3.gif"
-     pipe.style.position = "absolute"
-     pipe.style.bottom = "-2%"
-     bonecos2.style.display ="none"
-     pipe.style.width = "25%"
 }
 function trocab2(){
      var bonecos2 =document.querySelector(".bonecos2")
@@ -138,9 +131,25 @@ function trocab3(){
      var bonecos2 =document.querySelector(".bonecos2")
      pipe.src = "blas.gif"
      pipe.style.position = "absolute"
-     pipe.style.bottom = "-10%"
+     pipe.style.bottom = "0%"
      bonecos2.style.display ="none"
-     pipe.style.width = "25%"
+     pipe.style.width = "15%"
+}
+function trocab4(){
+     var bonecos2 =document.querySelector(".bonecos2")
+     pipe.src = "gordo.gif"
+     pipe.style.position = "absolute"
+     pipe.style.bottom = "-2%"
+     bonecos2.style.display ="none"
+     pipe.style.width = "15%"
+}
+function trocab5(){
+     var bonecos2 =document.querySelector(".bonecos2")
+     pipe.src = "mewtwo.gif"
+     pipe.style.position = "absolute"
+     pipe.style.bottom = "-2%"
+     bonecos2.style.display ="none"
+     pipe.style.width = "20%"
 }
 /*  
 function clica(){
@@ -157,11 +166,15 @@ function clica(){
      }, 100)
 }
  
- 
+ pipe.offsetLeft
 pipe1{
      position: absolute;
      bottom: -10%;    
      width: 25%;  
      right: 120px;     
-     animation
+     animation:
  */
+/*   if ( !charPosition < 30 &&  !pipePosition >= 460 ) {
+     pipe.style.animation = 'pipe1-animation 1.7s infinite linear; '
+     pipe.style.left =`${pipePosition}px`
+     }else  */
